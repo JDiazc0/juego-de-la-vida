b00f622953519a586bdf0e09f429989f595ee3b7
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-05-29 14:47:23 -0500

    Controles navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,27 @@ import "../utils/NavBar.css";
 export default function NavBar(props) {
   const {
     playButton,
-    pauseButton,
+    isPlaying,
+    isStepBack,
+    stepBack,
+    nextStep,
     clear,
-    slow,
-    fast,
     seed,
+    speed,
     gridSize,
     typeCell,
   } = props;
 
   const handleSelectGrid = (event) => {
-    gridSize(event.target.value);
+    gridSize(event);
   };
 
   const handleSelectTypeCell = (event) => {
-    typeCell(event.target.value);
+    typeCell(event);
+  };
+
+  const handleSelectSpeed = (event) => {
+    speed(event);
   };
 
   return (
@@ -29,12 +35,21 @@ export default function NavBar(props) {
           <ul className="nav-ul">
             <li>
               <button className="btn" onClick={playButton}>
-                Play
+                {isPlaying === true ? "Pause" : "Play"}
               </button>
             </li>
             <li>
-              <button className="btn" onClick={pauseButton}>
-                Pause
+              <button
+                className={`btn ${isStepBack ? "isStepBack" : ""}`}
+                onClick={stepBack}>
+                Step Back
+              </button>
+            </li>
+            <li>
+              <button
+                className={`btn ${isPlaying ? "playing" : ""}`}
+                onClick={nextStep}>
+                Next Step
               </button>
             </li>
             <li>
@@ -43,34 +58,40 @@ export default function NavBar(props) {
               </button>
             </li>
             <li>
-              <button className="btn" onClick={slow}>
-                Slow
-              </button>
-            </li>
-            <li>
-              <button className="btn" onClick={fast}>
-                Fast
-              </button>
-            </li>
-            <li>
-              <button className="btn" onClick={seed}>
+              <button
+                className={`btn ${isPlaying ? "playing" : ""}`}
+                onClick={seed}>
                 Seed
               </button>
             </li>
             <li>
-              <select className="select gridSize" onChange={handleSelectGrid}>
-                <option value={1}>20x10</option>
-                <option value={2}>50x30</option>
-                <option value={3}>70x50</option>
+              <select
+                className="select speed"
+                onChange={handleSelectSpeed}
+                defaultValue="1000">
+                <option value="1000">Normal</option>
+                <option value="2000">Slow</option>
+                <option value="500">Fast</option>
+              </select>
+            </li>
+            <li>
+              <select
+                className={`select gridSize ${isPlaying ? "playing" : ""}`}
+                onChange={handleSelectGrid}
+                defaultValue="1">
+                <option value="1">20x20</option>
+                <option value="2">50x40</option>
+                <option value="3">70x50</option>
               </select>
             </li>
             <li>
               <select
                 className="select typeCell"
-                onChange={handleSelectTypeCell}>
-                <option value={1}>Color Clase Baja</option>
-                <option value={2}>Color Clase Media</option>
-                <option value={3}>Color Clase Alta</option>
+                onChange={handleSelectTypeCell}
+                defaultValue="1">
+                <option value="1">Color Clase Baja</option>
+                <option value="2">Color Clase Media</option>
+                <option value="3">Color Clase Alta</option>
               </select>
             </li>
           </ul>
